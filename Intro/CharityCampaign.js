a70ea40f3f799c40e13f1f29d261b18dd39c5b77
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-17 21:27:39 +0300

    Add files to JS basics

---
+++ Intro/CharityCampaign.js
@@ -0,0 +1,9 @@
+function campaign(countDays, countcooks, countCakes, countGoffres, countPanCakes){
+    let totalCakesSum= +countCakes*45 *+countDays*+countcooks;
+    let totalGoffresSum= +countGoffres*5.80*+countcooks*+countDays;
+    let totalPancakesSum=+countPanCakes*3.20*+countcooks*+countDays;
+    let totalSum= (totalCakesSum+totalGoffresSum+totalPancakesSum)*0.875;
+    console.log(totalSum.toFixed(2));
+
+}
+campaign(23,8,14,30,16);
---
+++ Intro/FruitMarket.js
@@ -0,0 +1,9 @@
+function costs(strawberryPrice, quantityBananas, quantityOranges,quantityRaspberries, quantityStrawberries){
+    let priceRaspberries=+strawberryPrice*0.5;
+    let priceOranges=priceRaspberries*0.6;
+    let priceBananas=priceRaspberries*0.2;
+    let totalPrice= priceRaspberries*quantityRaspberries + strawberryPrice
+     *quantityStrawberries+quantityOranges*priceOranges+priceBananas*quantityBananas;
+     console.log(totalPrice);
+}
+costs(48,10,3.3,6.5,1.7);
